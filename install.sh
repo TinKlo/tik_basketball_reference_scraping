0ca20b72a867b2ea9d734fd06229845197f41d92
TinKlo <thiagoiklock@gmail.com>
2022-12-31 18:25:33 -0300

    creates install.sh with receipt for creating virtualenv.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+dt=$(date '+%d%m%Y %H%M%S')
+DAY=$(date +"%u")
+c=$DAY$dt
+d='.venv'
+f="${d}${c}"
+echo "running: " virtuaelnv --python=python3 ${f:0:8}
+virtualenv --python=python3  ${f:0:8}
+echo "activating..."
+echo source ${f:0:8}/bin activate
+source ${f:0:8}/bin activate
+echo ".venv activated"
