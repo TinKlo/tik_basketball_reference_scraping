2ef34eac901a227350feb45cd1cb784957829d3a
TinKlo <thiagoiklock@gmail.com>
2023-01-02 17:37:43 -0300

    adds counter and Magic Jhonso and Jakob poetle

--- player_season_stats.py
+++ player_season_stats.py
@@ -9,9 +9,10 @@ import pandas as pd
 logging.basicConfig(filename='player_season_stats.log', level=logging.INFO,datefmt='%Y-%m-%d %H:%M:%S', format='%(asctime)s %(levelname)-8s %(message)s' )
 logging.info('Starting...')
 
-players_list = ["doncilu01","abdulka01","mcgratr01"]
+players_list = ["doncilu01","abdulka01","mcgratr01","poeltja01","johnsma02"]
 
 def get_season_averages_per_player(players_code_input):
+    count = 0
     for p in players_code_input:
         logging.info('Starting run...')
         logging.info("Getting season stats for: "+str(p))
@@ -28,12 +29,13 @@ def get_season_averages_per_player(players_code_input):
         table_json = table_df.to_json(orient="columns")
         os.makedirs('folder/subfolder', exist_ok=True)
         currentDateAndTime = datetime.now()
-        filename1 = datetime.now().strftime("%Y%m%d-%H%M%S")+"_"+p
+        filename1 = datetime.now().strftime("%Y%m%d-%H")+"_"+p
         os.makedirs(f'folder/', exist_ok=True)
-        export_json2 = table_df.to_json(f'folder/subfolder/{filename1}.json')
+        # export_json2 = table_df.to_json(f'folder/subfolder/{filename1}.json')
         export_json2 = table_df.to_csv(f'folder/subfolder/{filename1}.csv')
-        logging.info("Finished for: "+str(p))
-        print(table_df)
+        logging.info("Extracted Season stats for: "+str(p))
+        count +=1
+        print(f"{count}.Finished for: "+str(p))
     logging.info("End of run.")
     return table_df
 
