d62080d71f2d65cea5d97c329034c70cd64bb73f
TinKlo <thiagoiklock@gmail.com>
2023-01-02 13:06:40 -0300

    adds iteration over files in folde

--- load.py
+++ load.py
@@ -2,34 +2,39 @@ import pandas as pd
 from sqlalchemy import create_engine
 import json
 import logging
+from datetime import date
+import os
 
 logging.basicConfig(filename='load.log', level=logging.INFO,datefmt='%Y-%m-%d %H:%M:%S', format='%(asctime)s %(levelname)-8s %(message)s' )
 logging.info('Starting...')
 
 cred = "credentials.json"
-data = "dados_prova.txt"
-
-
 # loading credentials
+with open(cred) as c:
+    logging.info('Fetching Credentials...')
+    cred = json.load(c)
+    logging.info('Credential loaded...')
 
-with open(cred) as f:
-    cred = json.load(f)
+today = date.today()
 
-
-def load_data_dataframe(data):
-    ''' loads data from local directory to the dataframe.
-        insert those rows to a postgres database according to the
-        credentials file'''
-    df = pd.read_csv("../subfolder/20221231-195033_mcgratr01.csv", header=0, sep=',', engine='python')
-    filtered_df = df[0].str.startswith(searched_term, na=False)
-    return df[filtered_df]
-
-
-def load_data_to_psql():
+def load_data_to_psql(folder):
+    logging.info('Creating Engine')
     engine = create_engine(cred["URL"])
-    df = pd.read_csv("/home/chic/deletavel/tik_basketball_reference_scraping/folder/subfolder/20221231-195033_mcgratr01.csv", header=None, sep=r'\s{2,}', engine='python')
-    df.to_sql("dados_prova1", engine,if_exists='append', chunksize=1000)
+    logging.info("Avaialble files in folder: " + str(os.listdir(folder)))
+    print(os.listdir(folder))
+    for filename in os.listdir(folder):
+        f = os.path.join(folder, filename)
+    # checking if it is a file
+        if os.path.isfile(f):
+            print(f)
+            df = pd.read_csv(f)
+            print(df)
+            table = "daily_seasonal_stats"
+            df.to_sql(table, engine,if_exists='append', chunksize=1000)
+            logging.info(f'Loaded {f} to database in table {table}')
+    logging.info('End of loading')
     return True
 
 
-load_data_to_psql()
+load_data_to_psql("./folder/subfolder")
+logging.info('Finished.')
